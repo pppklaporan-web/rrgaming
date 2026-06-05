c32949a537c5ccd2c2adafcfe0862ee435605686
Ryan Oktarino <pppklaporan@gmail.com>
2026-06-05 22:22:50 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,3 @@
-// service-worker.js
 self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim());
+  console.log('Service Worker terinstall');
 });
