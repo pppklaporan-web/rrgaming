20acd9853b01d9946c1f318c14dc1e2bef7a5645
Ryan Oktarino <pppklaporan@gmail.com>
2026-06-05 22:20:40 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,8 @@
 // service-worker.js
-const CACHE_NAME = "rr-gaming-v1";
-const ASSETS = ["/", "/index.html", "/kasir.html", "/owner.html", "/app.js"];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    }),
-  );
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    }),
-  );
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
 });
